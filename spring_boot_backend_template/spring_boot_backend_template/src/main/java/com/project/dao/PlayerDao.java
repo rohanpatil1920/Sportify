447148967fcac62e4f94c4244750415b38d89507
Ameya <maydeoameya22@gmail.com>
2025-01-29 17:16:06 +0530

    First push of Finalized Sportify Entities

---
+++ spring_boot_backend_template/spring_boot_backend_template/src/main/java/com/project/dao/PlayerDao.java
@@ -0,0 +1,11 @@
+package com.project.dao;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.pojos.Player;
+
+public interface PlayerDao extends JpaRepository<Player, Long> {
+	Optional<Player> findByEmailAndPassword(String email, String password);
+}
---
+++ spring_boot_backend_template/spring_boot_backend_template/src/main/java/com/project/pojos/Court.java
@@ -0,0 +1,52 @@
+package com.project.pojos;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "court")
+@ToString(callSuper = true, exclude = { "venue", "sport" })
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class Court extends SuperEntity {
+
+	@Column(name = "price_per_hour", precision = 10, scale = 2)
+	@DecimalMin(value = "0.0", inclusive = false)
+	private BigDecimal pricePerHour;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "venue_id", nullable = false)
+	private Venue venue;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "sport_id", nullable = false)
+	private Sport sport;
+
+	@OneToMany(mappedBy = "court", cascade = CascadeType.ALL, orphanRemoval = true)
+	private Set<Booking> bookings = new HashSet<>();
+
+	public Court(@DecimalMin(value = "0.0", inclusive = false) BigDecimal pricePerHour, Venue venue, Sport sport) {
+		super();
+		this.pricePerHour = pricePerHour;
+		this.venue = venue;
+		this.sport = sport;
+	}
+
+}
